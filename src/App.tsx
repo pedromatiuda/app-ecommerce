c9dca5d9886f7a8d4e17e0dc75463a78c6250b39
Pedro Spinelli DEV <pedromatiuda@gmail.com>
2023-05-23 18:32:46 -0300

    configurando eslint, prettier e styledCompontents

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: #ca1212;
+`;
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.container}>Testando</Text>
+      <TextNew>Novo Texto</TextNew>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+});
+
+export default App;
