08ae4409e25aa4a1c37c0fc3b7c8725fda8a555f
Pedro Spinelli DEV <pedromatiuda@gmail.com>
2023-07-27 18:50:28 -0300

    feat: install axios

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -6,7 +6,7 @@ import { theme } from '../../../shared/themes/theme';
 import { ContainerLogin, Imagelogo } from '../styles/login.style';
 
 const Login = () => {
-  const handleOnPress = () => {
+  const handleOnPress = async () => {
     console.log('clicou');
   };
   return (
