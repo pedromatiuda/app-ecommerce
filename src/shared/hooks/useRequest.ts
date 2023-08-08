e5547fa2ae3bf27369e0e2e6b0fcd918ac8e9169
Pedro Spinelli DEV <pedromatiuda@gmail.com>
2023-08-08 18:13:38 -0300

    feat: insert redux

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -1,20 +1,21 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { setUserAction } from '../../store/reducers/userReducer';
 import { connectionAPICPost } from '../functions/connection/connectionAPI';
 import { RequestLogin } from '../types/requestLogin';
 import { ReturnLogin } from '../types/returnLogin';
-import { UserType } from '../types/userType';
 
 export const useRequest = () => {
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
-  const [user, setUser] = useState<UserType>();
 
   const authRequest = async (body: RequestLogin) => {
     setLoading(true);
     await connectionAPICPost<ReturnLogin>('http://192.168.15.12:3000/auth', body)
       .then((result) => {
-        setUser(result.user);
+        dispatch(setUserAction(result.user));
       })
       .catch(() => {
         setErrorMessage('Usuário ou senha inválidos');
@@ -24,7 +25,6 @@ export const useRequest = () => {
 
   return {
     loading,
-    user,
     errorMessage,
     authRequest,
     setErrorMessage,
