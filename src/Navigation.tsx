96d81228984221f5beb4052ad61dc6cf526d4e73
Pedro Spinelli DEV <pedromatiuda@gmail.com>
2023-08-31 18:44:33 -0300

    feat: insert tab navigation with styles

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,13 +1,71 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer, ParamListBase, RouteProp } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 
 import CreateUser from './modules/createUser';
 import Home from './modules/home';
 import Login from './modules/login';
+import Orders from './modules/orders';
+import Profile from './modules/profile';
 import Splash from './modules/splash';
+import { Icon } from './shared/components/icon/Icon';
 import { MenuUrl } from './shared/enums/MenuUrl.enum';
+import { theme } from './shared/themes/theme';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  const renderTabBarIcon = (color: string, route: RouteProp<ParamListBase, string>) => {
+    let iconName: string;
+
+    switch (route.name) {
+      case 'Home':
+        iconName = 'home';
+        break;
+
+      case 'Orders':
+        iconName = 'cart';
+        break;
+
+      default:
+        iconName = 'profile';
+        break;
+    }
+
+    return <Icon size={16} name={iconName} color={color} />;
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => renderTabBarIcon(color, route),
+        tabBarActiveTintColor: theme.colors.mainTheme.primary,
+        tabBarInactiveTintColor: theme.colors.grayTheme.gray80,
+        tabBarLabelStyle: {
+          marginBottom: 8,
+        },
+        tabBarStyle: {
+          height: 52,
+          padding: 8,
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Tab.Screen
+        name="Orders"
+        component={Orders}
+        options={{ title: 'Pedidos', headerShown: false }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{ title: 'Perfil', headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Navigation = () => {
   return (
@@ -20,7 +78,11 @@ const Navigation = () => {
           component={CreateUser}
           options={{ title: 'Criar usuário' }}
         />
-        <Stack.Screen name={MenuUrl.HOME} component={Home} options={{ title: 'Home' }} />
+        <Stack.Screen
+          name={MenuUrl.HOME}
+          component={TabNavigation}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
